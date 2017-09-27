230574c47b1598dfaf99b844c68d4a87878ee0d1
Martijn van Gogh <martijn@traveladventuregames.nl>
2017-09-27 13:23:14 +0200

    initial commit

---
+++ ARExperiments/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARExperiments
+//
+//  Created by Martijn van Gogh on 27-09-17.
+//  Copyright © 2017 Martijn van Gogh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
